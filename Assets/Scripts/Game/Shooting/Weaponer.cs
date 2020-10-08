645a41e968011961d28e239d3d02d7a73d580c71
Ruslan Smirnov <russ944@gmail.com>
2020-10-08 18:35:47 +0300

    Created shoot animation

--- Assets/Scripts/Game/Shooting/Weaponer.cs
+++ Assets/Scripts/Game/Shooting/Weaponer.cs
@@ -1,14 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Weaponer : MonoBehaviour
 {
     [SerializeField] private Weapon weapon;
 
+    public event Action OnAppliedWeapon;
+    
     [ContextMenu("ApplyWeapon test")]
     public void ApplyWeapon()
     {
         weapon.Shoot();
+        OnAppliedWeapon?.Invoke();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ApplyWeapon();
+        }
     }
 }
