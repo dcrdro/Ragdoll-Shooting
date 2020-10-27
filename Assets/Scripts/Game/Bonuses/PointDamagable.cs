1031414526b463f854285033f7415de230a3ab26
Ruslan Smirnov <russ944@gmail.com>
2020-10-27 18:18:29 +0200

    Created damagable for hit-by-point damaging

---
+++ Assets/Scripts/Game/Bonuses/PointDamagable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointDamagable : MonoBehaviour, IDamagable
+{
+    [SerializeField] private Health health;
+    
+    public event Action<IDamagable, float> OnDamageTaken;
+    
+    public void TakeDamage(float damage)
+    {
+        health.TakeDamage(1);
+        OnDamageTaken?.Invoke(this, 1);
+    }
+}
