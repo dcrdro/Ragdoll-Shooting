f33d6f4bef29a27bb4e2562081b0d43b1e8cb6f2
Ruslan Smirnov <russ944@gmail.com>
2020-10-27 18:18:49 +0200

    Created adapter for receiving damage on collider

---
+++ Assets/Scripts/Game/TriggerDamagable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class TriggerDamagable : MonoBehaviour, IDamagable
+{
+    [SerializeField] private MonoBehaviour damagable; // rework
+
+    private IDamagable Damagable => (IDamagable) damagable;
+
+    public event Action<IDamagable, float> OnDamageTaken
+    {
+        add => Damagable.OnDamageTaken += value;
+        remove => Damagable.OnDamageTaken -= value;
+    }
+
+    public void TakeDamage(float damage) => Damagable.TakeDamage(damage);
+}
