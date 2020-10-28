eeee58b37ae8f921aef90b0c86683e3a38879a41
Ruslan Smirnov <russ944@gmail.com>
2020-10-28 17:10:04 +0200

    Introduced delayed destroyer

---
+++ Assets/Scripts/Game/Bonuses/DelayedDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DelayedDestroyer : MonoBehaviour
+{
+    [SerializeField] private float delay;
+
+    private void Start()
+    {
+        Destroy(gameObject, delay);
+    }
+}
