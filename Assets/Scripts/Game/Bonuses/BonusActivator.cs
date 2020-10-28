1f6a4888613bd0f326361c3383a049b21b5bdc56
Ruslan Smirnov <russ944@gmail.com>
2020-10-28 16:30:29 +0200

    Added shield bonus activation stub logic

---
+++ Assets/Scripts/Game/Bonuses/BonusActivator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BonusActivator : MonoBehaviour
+{
+    [SerializeField] private Health selfHealth; // rework
+
+    private IHealth SelfHealth => (IHealth) selfHealth;
+
+    private void OnEnable()
+    {
+        SelfHealth.OnDied += OnSelfDied;
+    }
+
+    private void OnDisable()
+    {
+        SelfHealth.OnDied -= OnSelfDied;
+    }
+
+    private void OnSelfDied(DeathArgs deathArgs)
+    {
+        // activate
+        Destroy(gameObject);
+    }    
+}
