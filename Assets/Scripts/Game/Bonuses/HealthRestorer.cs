a3800bd640739cf61cf63c3210562eb84c558920
Ruslan Smirnov <russ944@gmail.com>
2020-10-28 15:42:43 +0200

    Added health restorer component for Medicine Kit

---
+++ Assets/Scripts/Game/Bonuses/HealthRestorer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class HealthRestorer : MonoBehaviour
+{
+    [SerializeField] private Health selfHealth; // rework
+    [SerializeField] private float restoreAmount;
+
+    private IHealth SelfHealth => (IHealth) selfHealth;
+
+    private void OnEnable()
+    {
+        SelfHealth.OnDied += OnSelfDied;
+    }
+
+    private void OnDisable()
+    {
+        SelfHealth.OnDied -= OnSelfDied;
+    }
+
+    private void OnSelfDied(DeathArgs deathArgs)
+    {
+        GameObject restoreTarget = deathArgs.Origin;
+        if (restoreTarget.TryGetComponent<IHealable>(out var healable))
+        {
+            healable.TakeHeal(new HealArgs(gameObject, gameObject, restoreAmount));
+        }
+    }
+}
