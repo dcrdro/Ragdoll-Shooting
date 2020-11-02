9f8d6e1edd9657424181e51ea1021ccbc61c91f3
Ruslan Smirnov <russ944@gmail.com>
2020-11-02 15:31:27 +0200

    Added destroyer based on life time

---
+++ Assets/Scripts/Game/Bonuses/LifeTimedDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LifeTimedDestroyer : MonoBehaviour
+{
+    [SerializeField] private LifeTimer lifeTimer;
+
+    void OnEnable() => lifeTimer.OnEnded += OnTimeEnded;
+    void OnDisable() => lifeTimer.OnEnded -= OnTimeEnded;
+
+    private void OnTimeEnded() => Destroy(gameObject);
+}
