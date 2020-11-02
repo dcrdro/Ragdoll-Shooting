2a891ad256c807fc3e0ce5d2d04840e64704fc27
Ruslan Smirnov <russ944@gmail.com>
2020-11-02 10:20:46 +0200

    Added MultiTargetDamager

---
+++ Assets/Scripts/Core/IMultiTargetDamager.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IMultiTargetDamager
+{
+     event Action<IEnumerable<IDamagable>> OnDamaged;
+     
+     void Damage(IEnumerable<IDamagable> damagables);
+}
