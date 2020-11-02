20da972f3343d10aaeb32870a0aac05118f98450
Ruslan Smirnov <russ944@gmail.com>
2020-11-02 16:53:49 +0200

    Add fighter's behavior based on game over

---
+++ Assets/Scripts/Game/ControlDisabler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class ControlDisabler : MonoBehaviour
+{
+    [SerializeField] private UnitControllerBase unitController;
+    [SerializeField] private GameOverManager gameOverManager;
+
+    private void OnEnable() => gameOverManager.OnGameOver += OnGameOver;
+    private void OnDisable() => gameOverManager.OnGameOver -= OnGameOver;
+
+    private void OnGameOver() => unitController.enabled = false;
+}
