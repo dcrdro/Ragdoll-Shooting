ecd9de76c48996aa8190ac9080f7fba7c6c01ce1
Ruslan Smirnov <russ944@gmail.com>
2020-11-02 15:10:09 +0200

    Adapted bomb delay destroyer to timable

--- Assets/Scripts/Game/Bonuses/TimedExplosion.cs
+++ Assets/Scripts/Game/Bonuses/TimedExplosion.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 public class TimedExplosion : MonoBehaviour
 {
     [SerializeField] private ExplosionCollidable explosionCollidable;
-    [SerializeField] private float explosionDelay;
+    [SerializeField] private LifeTimer lifeTimer;
 
-    // Start is called before the first frame update
-    IEnumerator Start()
-    {
-        yield return new WaitForSeconds(explosionDelay);
-        explosionCollidable.OnCollide(null); // rework
-    }
+    void OnEnable() => lifeTimer.OnEnded += OnTimeEnded;
+    void OnDisable() => lifeTimer.OnEnded -= OnTimeEnded;
+
+    private void  OnTimeEnded() => explosionCollidable.OnCollide(null);
 }
---
+++ Assets/Scripts/Game/LifeTimer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class LifeTimer : MonoBehaviour, ITimable
+{
+    [SerializeField] private float lifeTime;
+
+    private float remainTime;
+    private bool isEnded;
+
+    public float Capacity => lifeTime;
+    public float Current => remainTime;
+    
+    public event Action OnEnded;
+
+    private void Awake() => remainTime = lifeTime;
+
+    // TODO: replace on update callback
+    private void Update()
+    {
+        if (isEnded) return;
+        
+        remainTime -= Time.deltaTime;
+        if (remainTime <= 0)
+        {
+            isEnded = true;
+            OnEnded?.Invoke();
+        }
+    }
+}
