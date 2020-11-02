75e403c231f574cd810a5035d94f21db678c91c2
Ruslan Smirnov <russ944@gmail.com>
2020-11-02 17:26:38 +0200

    Introduced UI Manager

---
+++ Assets/Scripts/Game/UI/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameOverManager gameOverManager;
+    [SerializeField] private GameObject gameOverPanel;
+
+    private void OnEnable() => gameOverManager.OnGameOver += OnGameOver;
+
+    private void OnDisable() => gameOverManager.OnGameOver -= OnGameOver;
+
+    private void OnGameOver() => StartCoroutine(WaitForShowGameOver());
+
+    private IEnumerator WaitForShowGameOver()
+    {
+        yield return new WaitForSeconds(1f);
+        gameOverPanel.SetActive(true);
+    }
+}
