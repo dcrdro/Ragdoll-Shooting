a78465dfccdeec1bd8a16b67461af50ba0fdce9d
Ruslan Smirnov <russ944@gmail.com>
2020-11-02 11:52:44 +0200

    Added spawner of bonuses

---
+++ Assets/Scripts/Game/BonusesFactory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BonusesFactory : MonoBehaviour, IFactory<int, GameObject>
+{
+    [SerializeField] private GameObject[] bonusesPrefabs;
+    
+    public GameObject Produce(int id) => Instantiate(bonusesPrefabs[id]);
+    
+    public int Size => bonusesPrefabs.Length;
+}
---
+++ Assets/Scripts/Game/BonusesSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using Unity.Jobs.LowLevel.Unsafe;
+using UnityEngine;
+
+public class BonusesSpawner : MonoBehaviour
+{
+    [SerializeField] private MonoBehaviour factory; // as IFactory<int, GameObject>
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float spawnInterval;
+
+    // for removing dependency on a specific factory
+    private IFactory<int, GameObject> Factory => (IFactory<int, GameObject>) factory;
+
+    private IEnumerator Start()
+    {
+        WaitForSeconds spawnWait = new WaitForSeconds(spawnInterval);
+        while (true)
+        {
+            yield return spawnWait;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        int randomIndex = Random.Range(0, Factory.Size);
+        GameObject product = Factory.Produce(randomIndex);
+
+        product.transform.parent = spawnPoint;
+        product.transform.localPosition = Vector3.zero;
+    }
+}
