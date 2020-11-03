3f7ff88f66f40979488222436ee7e453c81df163
Ruslan Smirnov <russ944@gmail.com>
2020-11-03 09:17:07 +0200

    Added pause button & panel UI

---
+++ Assets/Scripts/Game/UI/PausePanelUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PausePanelUI : UIBase
+{
+    [SerializeField] private AppManager appManager;
+    
+    // UI Event
+    public void OnResume() => appManager.UnpauseGame();
+    public void OnRestart() => appManager.RestartGame();
+}
