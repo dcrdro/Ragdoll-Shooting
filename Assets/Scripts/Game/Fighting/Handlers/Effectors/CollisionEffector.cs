090f567f5b6900565e6e293de35fd57466dd57fe
Ruslan Smirnov <russ944@gmail.com>
2020-11-04 17:49:46 +0200

    Created collision effector

---
+++ Assets/Scripts/Game/Fighting/Handlers/Effectors/CollisionEffector.cs
@@ -0,0 +1,27 @@
+﻿using Core.Effects;
+using Game.Effects;
+using Game.Interacting;
+using UnityEngine;
+
+namespace Game.Fighting.Handlers
+{
+    public class CollisionEffector : EffectorBase<PositionedArgs>
+    {
+        [SerializeField] private CollisionSender2D sender;
+        private void OnEnable()
+        {
+            sender.OnEnter += OnCollision;        
+        }
+
+        private void OnDisable()
+        {
+            sender.OnEnter -= OnCollision;        
+        }
+        
+        private void OnCollision(Collision2D obj)
+        {
+            PositionedArgs effectArgs = new PositionedArgs(obj.GetContact(0).point, Quaternion.identity);
+            PlayEffects(effectArgs);
+        }
+    }
+}
