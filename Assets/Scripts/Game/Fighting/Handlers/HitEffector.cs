06998021d4626de93fbd44d49e73680941b74752
Ruslan Smirnov <russ944@gmail.com>
2020-11-04 16:28:33 +0200

    Added HitEffector for handling hits & effects

---
+++ Assets/Scripts/Game/Fighting/Handlers/HitEffector.cs
@@ -0,0 +1,40 @@
+﻿using Core.Effects;
+using Core.Fighting;
+using Core.Fighting.Args;
+using Game.Effects;
+using Game.Fighting.Hitboxes;
+using UnityEngine;
+
+namespace Game.Fighting.Handlers
+{
+    public class HitEffector : EffectorBase<PositionedArgs>
+    {
+        [SerializeField] private HitReceiver[] hitReceivers;
+        [SerializeField] private GameObject rootObject;
+    
+        private void OnValidate() => hitReceivers = rootObject.GetComponentsInChildren<HitReceiver>();
+
+        private void OnEnable()
+        {
+            foreach (var receiver in hitReceivers)
+            {
+                receiver.DamageTaken += OnDamageTaken;
+            }        
+        }
+
+        private void OnDisable()
+        {
+            foreach (var receiver in hitReceivers)
+            {
+                receiver.DamageTaken -= OnDamageTaken;
+            }        
+        }
+
+        private void OnDamageTaken(IDamagable source, DamageArgs args)
+        {
+            HitReceiver receiver = (HitReceiver) source;
+            PositionedArgs effectArgs = new PositionedArgs(receiver.transform.position, Quaternion.identity);
+            PlayEffects(effectArgs);
+        }
+    }
+}
