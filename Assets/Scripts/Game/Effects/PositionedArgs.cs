7c3010bdb9eec0ac8d1a8407cf1735214d643abb
Ruslan Smirnov <russ944@gmail.com>
2020-11-04 16:28:07 +0200

    Added decorator positioned effect

---
+++ Assets/Scripts/Game/Effects/PositionedArgs.cs
@@ -0,0 +1,17 @@
+﻿using Core.Effects;
+using UnityEngine;
+
+namespace Game.Effects
+{
+    public readonly struct PositionedArgs : IEffectArgs
+    {
+        public readonly Vector3 Position;
+        public readonly Quaternion Rotation;
+
+        public PositionedArgs(Vector3 position, Quaternion rotation)
+        {
+            Position = position;
+            Rotation = rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Effects/PositionedEffect.cs
@@ -0,0 +1,16 @@
+﻿using Core.Effects;
+using UnityEngine;
+
+namespace Game.Effects
+{
+    public class PositionedEffect : EffectBase<PositionedArgs>
+    {
+        [SerializeField] private EmptyEffect effect;
+    
+        public override void Play(in PositionedArgs args)
+        {
+            effect.transform.SetPositionAndRotation(args.Position, args.Rotation);
+            effect.Play(EmptyArgs.Empty);
+        }
+    }
+}
