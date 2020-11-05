0d63c750cac02e11fa1e51a8f266451110dd5354
Ruslan Smirnov <russ944@gmail.com>
2020-11-05 11:50:16 +0200

    Added destroy effect decorator

---
+++ Assets/Scripts/Game/Effects/Decorators/DestroyEffect.cs
@@ -0,0 +1,17 @@
+﻿using Core.Effects;
+using UnityEngine;
+
+namespace Game.Effects
+{
+    public class DestroyEffect : EmptyEffect
+    {
+        [SerializeField] private EmptyEffect effect;
+        [SerializeField] private float delay;
+    
+        public override void Play(in EmptyArgs args)
+        {
+            effect.Play(EmptyArgs.Empty);
+            Destroy(effect.gameObject, delay);
+        }
+    }
+}
