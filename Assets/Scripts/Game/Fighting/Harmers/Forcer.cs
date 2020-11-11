962f89de91ac3c8e751880df3d546aa04675a79e
Ruslan Smirnov <russ944@gmail.com>
2020-11-11 10:12:22 +0200

    Added Forcer component

---
+++ Assets/Scripts/Game/Fighting/Harmers/Forcer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Core.Fighting;
+using Core.Fighting.Args;
+using Core.General;
+using UnityEngine;
+
+namespace Game.Fighting.Damagers
+{
+    public class Forcer : MonoBehaviour, IForcer, IOriginDerived
+    {
+        [SerializeField] private float force;
+        [SerializeField] private Transform viewRoot;
+    
+        public event Action<IForcer> Forced;
+        
+        public GameObject Origin { get; set; }
+
+        public void Force(IForceable forceable)
+        {
+            forceable.ApplyForce(new ForceArgs(Origin, gameObject, viewRoot.right * force));
+            Forced?.Invoke(this);
+        }
+    }
+}
