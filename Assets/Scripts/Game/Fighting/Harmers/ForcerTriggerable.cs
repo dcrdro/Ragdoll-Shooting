6fa9348f4572d229b7837bbaf88805bef54af216
Ruslan Smirnov <russ944@gmail.com>
2020-11-11 10:17:22 +0200

    Applied force behavior to bullet

---
+++ Assets/Scripts/Game/Fighting/Harmers/ForcerTriggerable.cs
@@ -0,0 +1,26 @@
+﻿using Core.Fighting;
+using Core.Interacting;
+using UnityEngine;
+
+namespace Game.Fighting.Damagers
+{
+    public class ForcerTriggerable : MonoBehaviour, ITriggerable
+    {
+        [SerializeField] private MonoBehaviour forcer; // rework
+        [SerializeField] private LayerMask forceMask;
+    
+        private IForcer Forcer => (IForcer) forcer;
+
+        public LayerMask TriggerableLayer => forceMask;
+
+        public void OnTrigger(Collider2D collider)
+        {
+            if (collider.TryGetComponent<IForceable>(out var forceable))
+            {
+                
+                print("apply force: " + collider.name);
+                Forcer.Force(forceable);
+            }
+        }
+    }
+}
