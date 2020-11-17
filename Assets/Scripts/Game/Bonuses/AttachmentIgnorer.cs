b9c1aeea7903c5952fb771b8f5dde3952cebcb26
Ruslan Smirnov <russ944@gmail.com>
2020-11-17 09:55:49 +0200

    Introduced and applied AttachmentIgnorer

---
+++ Assets/Scripts/Game/Bonuses/AttachmentIgnorer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Core.General;
+using Game.Fighting.Shooting;
+using UnityEngine;
+
+namespace Game.Bonuses
+{
+    public class AttachmentIgnorer : MonoBehaviour, IOriginDerived
+    {
+        [SerializeField] private Collider2D[] attachmentColliders;
+        
+        public GameObject Origin { get; set; }
+
+        private WeaponBase currentWeapon;
+        
+        private void Start()
+        {
+            currentWeapon = Origin.GetComponent<Weaponer>().Weapon;
+            currentWeapon.Shot += OnShot;
+        }
+
+        private void OnDisable()
+        {
+            currentWeapon.Shot -= OnShot;
+        }
+
+        private void OnShot(GameObject instance)
+        {
+            Collider2D[] instanceColliders = instance.GetComponentsInChildren<Collider2D>();
+            foreach (var instanceCollider in instanceColliders)
+            {
+                foreach (var attachmentCollider in attachmentColliders)
+                {
+                    Physics2D.IgnoreCollision(instanceCollider, attachmentCollider);
+                }
+            }
+        }
+    }
+}
